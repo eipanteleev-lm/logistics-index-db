5ae84030c6915640f55ef2131a5c6be800059e0c
Evgenii Panteleev 60065265 <evgenii.panteleev@leroymerlin.ru>
2020-07-21 15:17:55 +0300

    test fix

--- data/prices.sql
+++ data/prices.sql
@@ -3,4 +3,4 @@
 --changeset eipanteleev-lm:insert:prices
 
 insert into price (store_id, product_code, selling_price, storage_cost, purchase_price) 
-values (10, 18122532, 64, 0.62, NULL);
+values (10, 12345678, 64, 0.62, NULL);
